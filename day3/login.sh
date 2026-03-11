265b69700aede6f1190cd69f0d524a60a4b6f4d0
Bhuvaneswari-tech <bhuvanagitrepo@gmail.com>
2026-03-11 14:27:21 +0530

    Fourth commit

--- day3/login.sh
+++ day3/login.sh
@@ -9,44 +9,100 @@ password=$2
 if [[ ! $username =~ ^[a-zA-z]+$ ]]
 then
 	echo "Invalid username. username must be letters only"
-	return
+	return 1
 fi
 
 #password length checks
 if [[ ${#password} -lt 8 ]]
 then
 	echo "Password should be atleast 8 characters"
-	return
+	return 1
 fi
 
 #password must contain letters
 if [[ ! $password =~ [a-zA-z] ]]
 then
 	echo "Password must contain letters."
-	return
+	return 1
 fi
 
 #password must contain number
 if [[ ! $password =~ [0-9] ]]
 then
 	echo "Password must contain numbers"
-	return
+	return 1
 fi
 
 #password must contain one special character
 if [[ ! $password =~ [@#\$*^\&] ]]
 then
 	echo "Password must contain one special character."
-	return
+	return 1
 fi
 
-echo "Login credenticals are valid."
+#username and password from users.txt
+if grep -q "$username $password" users.txt
+then
+	echo "Login credentials are valid"
+else
+	echo "Invalid username or password"
+	return 1 
+fi
 }
 
-echo "Enter username: "
-read username
+#Login attempts
+attempt=1
+max_attempt=3
 
-echo "Enter password: "
-read password
+while [[ $attempt -le $max_attempt ]]
+do
+	echo "Enter username"
+	read username
 
-validate_login "$username" "$password"
+	echo "Enter password"
+	read password
+
+	validate_login "$username" "$password"
+
+	if [ $? -eq 0 ]
+	then
+		exit
+	fi
+
+	((attempt++))
+done
+
+echo "Account locked after 3 attempts"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
